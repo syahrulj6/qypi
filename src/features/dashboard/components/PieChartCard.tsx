d7063d4cf5ee8808a62d593049ae00d25510750f
syahrul jay <syahruljaylani73@gmail.com>
2025-03-15 11:32:40 +0700

    ui: creating pie chart card

---
+++ src/features/dashboard/components/PieChartCard.tsx
@@ -0,0 +1,101 @@
+// components/PieChartCard.tsx
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+} from "~/components/ui/card";
+import { PieChart, Pie, Label, Tooltip } from "recharts";
+import { ChartContainer, ChartTooltipContent } from "~/components/ui/chart";
+import { TrendingUp } from "lucide-react";
+
+interface PieChartCardProps {
+  data: { name: string; value: number; fill: string }[];
+  totalActivities: number;
+  config: {
+    [key: string]: {
+      label: string;
+      color: string;
+    };
+  };
+}
+
+export const PieChartCard = ({
+  data,
+  totalActivities,
+  config,
+}: PieChartCardProps) => {
+  return (
+    <div className="h-full flex-1">
+      <Card className="flex h-fit flex-col md:h-full">
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Activities Breakdown</CardTitle>
+          <CardDescription>By Activity Type</CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={config}
+            className="mx-auto aspect-square max-h-[250px]"
+          >
+            <PieChart>
+              <Tooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={data}
+                dataKey="value"
+                nameKey="name"
+                innerRadius={60}
+                outerRadius={100}
+                paddingAngle={5}
+                strokeWidth={0}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          textAnchor="middle"
+                          dominantBaseline="middle"
+                        >
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {totalActivities.toLocaleString()}
+                          </tspan>
+                          <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                          >
+                            Activities
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 text-sm">
+          <div className="flex items-center gap-2 font-medium leading-none">
+            5.2% activities increased this month{" "}
+            <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="leading-none text-muted-foreground">
+            Showing total activities by type
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
