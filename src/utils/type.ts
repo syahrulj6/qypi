75d07511b8485b42612f9ff9560ac086d915fb71
syahrul jay <syahruljaylani73@gmail.com>
2025-03-15 11:36:21 +0700

    type for chart activity

---
+++ src/utils/type.ts
@@ -0,0 +1,53 @@
+export type ChartActivityConfig = {
+  count: {
+    label: string;
+    color: string;
+  };
+  INBOX_RECEIVED: {
+    label: string;
+    color: string;
+  };
+  INBOX_CREATED: {
+    label: string;
+    color: string;
+  };
+  NOTE_CREATED: {
+    label: string;
+    color: string;
+  };
+  NOTEBOOK_CREATED: {
+    label: string;
+    color: string;
+  };
+  EVENT_CREATED: {
+    label: string;
+    color: string;
+  };
+};
+
+export const chartActivityConfig: ChartActivityConfig = {
+  count: {
+    label: "Activities",
+    color: "#4D55CC",
+  },
+  INBOX_RECEIVED: {
+    label: "Inbox Received",
+    color: "#8884d8",
+  },
+  INBOX_CREATED: {
+    label: "Inbox Sent",
+    color: "#82ca9d",
+  },
+  NOTE_CREATED: {
+    label: "Note Created",
+    color: "#ffc658",
+  },
+  NOTEBOOK_CREATED: {
+    label: "Notebook Created",
+    color: "#afa642",
+  },
+  EVENT_CREATED: {
+    label: "Event Created",
+    color: "#ff8042",
+  },
+};
