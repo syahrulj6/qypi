fde622c0a722da91d7710f22aa33a7a0d4f61a3d
syahrul jay <syahruljaylani73@gmail.com>
2025-03-16 15:40:37 +0700

    ui: responsive card

--- src/features/dashboard/components/MetricsCard.tsx
+++ src/features/dashboard/components/MetricsCard.tsx
@@ -1,3 +1,4 @@
+import { Info } from "lucide-react";
 import { Card } from "~/components/ui/card";
 
 interface MetricsCardProps {
@@ -16,10 +17,7 @@ export const MetricsCard = ({
   onClick,
 }: MetricsCardProps) => {
   return (
-    <Card
-      onClick={onClick}
-      className="flex flex-col items-center justify-center gap-4 rounded-lg border bg-card px-4 py-6 hover:cursor-pointer md:flex-row md:gap-6"
-    >
+    <Card className="flex flex-col items-center justify-center gap-4 rounded-lg border bg-card px-4 py-6 md:flex-row md:gap-6">
       <div
         className={`h-fit w-fit rounded-full p-3 md:h-14 md:w-14 md:p-4 ${iconBg ? iconBg : "bg-muted-foreground"}`}
       >
@@ -28,6 +26,12 @@ export const MetricsCard = ({
       <div className="flex flex-col justify-between text-center md:text-left">
         <h5 className="text-sm font-semibold md:text-base">{value}</h5>
         <p className="text-sm text-muted-foreground md:text-base">{title}</p>
+      </div>
+      <div
+        className="relative bottom-32 left-24 md:bottom-10 md:left-1"
+        onClick={onClick}
+      >
+        <Info className="h-5 w-5 text-muted-foreground transition-colors hover:cursor-pointer hover:text-current" />
       </div>
     </Card>
   );
