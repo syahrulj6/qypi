320719281cb1d525390b2fba3a8ceec99fc20cc6
syahrul jay <syahruljaylani73@gmail.com>
2025-03-17 08:50:58 +0700

    initial be for team management

--- src/pages/dashboard/team/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from "react";
-import DashboardLayout from "~/components/layout/DashboardLayout";
-
-export const Layout = ({ children }: { children: ReactNode }) => {
-  return <DashboardLayout>{children}</DashboardLayout>;
-};
-
-export default Layout;
---
+++ src/server/api/routers/team.ts
@@ -0,0 +1,1 @@
+import { createTRPCRouter, privateProcedure } from "../trpc";
